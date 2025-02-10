b0db2a52318a838bd04ce6701cb90325957274f0
CastrilloRedondoVictor <castrilloredondovictor@gmail.com>
2025-02-10 12:39:10 +0100

    Add project files.

---
+++ NetCoreLinqToSqlInjection/Controllers/CochesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreLinqToSqlInjection.Models;
+
+namespace NetCoreLinqToSqlInjection.Controllers
+{
+    public class CochesController : Controller
+    {
+
+        private ICoche coche;
+
+        public CochesController(ICoche coche)
+        {
+            this.coche = coche;
+        }
+        public IActionResult Index()
+        {
+            return View(this.coche);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string accion)
+        {
+            if(accion.ToLower() == "acelerar")
+            {
+                this.coche.Acelerar();
+            } else
+            {
+                this.coche.Frenar();
+            }
+            return View(this.coche);
+        }
+    }
+}
---
+++ NetCoreLinqToSqlInjection/Repositories/RepositoryDoctoresSQLServer.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace NetCoreLinqToSqlInjection.Repositories
+{
+    public class RepositoryDoctoresSQLServer
+    {
+        private DataTable tablaDoctores;
+        private SqlConnection _connection;
+        private SqlCommand _command;
+
+        public RepositoryDoctoresSQLServer()
+        {
+            string connectionString = @"Data Source=LOCALHOST\DESARROLLO;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Encrypt=True;Trust Server Certificate=True";
+            string sql = "select * from DOCTOR";
+            SqlDataAdapter ad = new SqlDataAdapter(sql, connectionString);
+            this.tablaDoctores = new DataTable();
+            ad.Fill(this.tablaDoctores);
+
+            this._connection = new SqlConnection(connectionString);
+            this._command = new SqlCommand();
+            this._command.Connection = this._connection;
+        }
+    }
+}
